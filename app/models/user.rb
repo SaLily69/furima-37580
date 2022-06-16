49f325b96e3eb9e1d39334dad60abab6bc6d8914
SaLily69 <Hyuri.proS8@gmail.com>
2022-06-16 20:09:16 +0900

    漢字・カナのみのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/} do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
   validates :birth_day, presence: true
 
 end
