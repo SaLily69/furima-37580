bff998ca140476a1f50435f187cf9bd670a6289e
SaLily69 <Hyuri.proS8@gmail.com>
2022-06-23 21:47:53 +0900

    ActiveHash導入、カテゴリー欄の表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :product_category
+
   has_one_attached :image
   
   validates :product_name,         presence: true, unless: :was_attached?
   validates :product_text,         presence: true
-  validates :product_category_id,  presence: true
+  validates :product_category_id,  numericality: { other_than: 1 , message: "can't be blank"}
   validates :product_condition_id, presence: true
   validates :shipping_cost_id,     presence: true
   validates :prefecture_id,        presence: true
