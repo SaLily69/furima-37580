ce643b231d3b7fa201afde173bae3cbf597cb04c
SaLily69 <Hyuri.proS8@gmail.com>
2022-06-25 11:39:45 +0900

    ActiveHashの入力項目全て実装
    ・商品状態
    ・送料負担
    ・発送元地域
    ・発送日数

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,20 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :product_category
+  belongs_to :product_condition
+  belongs_to :shipping_cost
+  belongs_to :prefecuture
+  belongs_to :shipping_day
 
   has_one_attached :image
   
   validates :product_name,         presence: true, unless: :was_attached?
   validates :product_text,         presence: true
   validates :product_category_id,  numericality: { other_than: 1 , message: "can't be blank"}
-  validates :product_condition_id, presence: true
-  validates :shipping_cost_id,     presence: true
-  validates :prefecture_id,        presence: true
-  validates :shipping_day_id,      presence: true
+  validates :product_condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_cost_id,     numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id,        numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id,      numericality: { other_than: 1 , message: "can't be blank"}
   validates :price,                presence: true
 
   def was_attached?
