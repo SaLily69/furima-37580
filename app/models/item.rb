7a4e69d80279d4aedbde5ededa2009f289a44bc9
SaLily69 <Hyuri.proS8@gmail.com>
2022-06-25 12:02:53 +0900

    priceのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,10 @@ class Item < ApplicationRecord
   validates :shipping_cost_id,     numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id,        numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_day_id,      numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price,                presence: true
+
+  with_options presence: true, numericality:{in:300..9999999}, format: { with: /\A[0-9]+\z/} do
+    validates :price
+  end
 
   def was_attached?
     self.image.attached?
