e81add71cf35a2f85594915ba143b0f88a392c40
SaLily69 <Hyuri.proS8@gmail.com>
2022-07-03 20:52:59 +0900

    商品削除機能の実装
    ・ルーティング、コントローラー、ビューの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,6 +36,11 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :product_name, :product_text, :product_category_id, :product_condition_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
