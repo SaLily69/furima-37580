7017b5d905982a5c958ca7aaa7e12309bc719531
SaLily69 <Hyuri.proS8@gmail.com>
2022-07-09 12:05:05 +0900

    purchaseコントローラーのcreate記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,6 +9,19 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @purchase = Purchase.new(purchase_params)
+    if @purchase.valid?
+      @purchase.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase).permit(:item_id, :user_id)
   end
 
 end
