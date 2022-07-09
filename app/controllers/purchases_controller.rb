a7fde1a6353166783e403a9949a0c0898c981d8f
SaLily69 <Hyuri.proS8@gmail.com>
2022-07-09 11:32:13 +0900

    purchasesコントローラー修正
    ・出品者が購入できないよう条件分岐記述
    他、
    ・モデルのアソシエーション記述
    ・purchsesをitemのネストにルーティング修正
    ・ビューファイルでitemを表示

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,11 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
 
   def index
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def create
