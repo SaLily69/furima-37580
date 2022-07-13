b9561a341f36f4f56080e235ae73921c49049581
SaLily69 <Hyuri.proS8@gmail.com>
2022-07-13 19:30:27 +0900

    API導入、購入機能実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -10,8 +10,10 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    binding.pry
     @purchase_shipping = PurchaseShipping.new(purchase_params)
     if @purchase_shipping.valid?
+      pay_item
       @purchase_shipping.save
       return redirect_to root_path
     else
@@ -22,11 +24,20 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_shipping).permit(:post_number, :prefecture_id, :city, :address, :house_number, :tel, :purchase_id).merge(item_id: params[:item_id], user_id: current_user.id)
+    params.require(:purchase_shipping).permit(:post_number, :prefecture_id, :city, :address, :house_number, :tel, :purchase_id).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
   end
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -1,16 +1,17 @@
 class PurchaseShipping
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :post_number, :prefecture_id, :city, :address, :house_number, :tel, :purchase_id
+  attr_accessor :item_id, :user_id, :post_number, :prefecture_id, :city, :address, :house_number, :tel, :purchase_id, :token
 
   with_options presence: true do
-    #validates :item_id
-    #validates :user_id
+    validates :item_id
+    validates :user_id
     validates :post_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :address
     validates :tel, numericality: { only_integer: true }, length: { in: 10..11 }
     validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
     #validates :purchase_id
+    validates :token
   end
 
   def save
